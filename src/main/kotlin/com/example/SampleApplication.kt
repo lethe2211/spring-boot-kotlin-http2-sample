3bfcd41025fbe9a33e10be0a375145cbe8a0889a
lethe2211 <lethe2211@gmail.com>
2022-02-13 14:25:51 +0900

    Add a sample controller

--- src/main/kotlin/com/example/SampleApplication.kt
+++ src/main/kotlin/com/example/SampleApplication.kt
@@ -2,10 +2,20 @@ package com.example
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class SampleApplication
 
 fun main(args: Array<String>) {
 	runApplication<SampleApplication>(*args)
+}
+
+@RestController
+class SampleController {
+	@GetMapping("/ok")
+	fun ok(): String {
+		return "OK"
+	}
 }
